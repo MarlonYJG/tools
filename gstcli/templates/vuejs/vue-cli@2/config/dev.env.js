b233f961b8631d6f3d752c7f032098b29e3ccec0
杨建国(10030252) <yangjg-b@glodon.com>
2022-06-07 11:06:54 +0800

    .

--- gstcli/templates/vuejs/vue-cli@2/config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const releaseEnv = require('./release.env')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(releaseEnv, prodEnv, {
-  NODE_ENV: '"development"'
-})
--- gstcli/templates/vuejs/vue-cli@2/src/store/mutation-types.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Marlon
- * @Date: 2020-03-14 11:40:30
- * @LastEditors: Marlon
- * @Description: 
- */
-
-// Demo
-export const GET_M_T_DEMO = 'GET_M_T_DEMO'; 
